1efd5c233dae9dd9acf50294cea50ba5cd1a9414
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-04-17 23:35:10 +0400

    edited rdt_sender

--- rdt2.0/src/rdt_sender.c
+++ rdt2.0/src/rdt_sender.c
@@ -218,7 +218,7 @@ int main (int argc, char **argv)
 
             //if you received an ack, 
             shift = ( recvpkt->hdr.ackno - window_base ) / DATA_SIZE ; 
-            printf( "just received a shift of %d  \n", shift );
+            printf( "just received a shift of %d from ack nuumber %d while th window_base is %d \n", shift, recvpkt->hdr.ackno, window_base );
 
         }while(recvpkt->hdr.ackno != next_seqno); //next_seq_no is largest packet number in window
         printf( "shift value leaving the loop is %d  \n", shift );
