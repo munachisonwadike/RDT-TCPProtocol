ed59a51c35aa07544c469f7d97f62dcfb0ace8b9
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-04-19 00:48:32 +0400

    here we are

--- rdt2.0/src/rdt_receiver.c
+++ rdt2.0/src/rdt_receiver.c
@@ -218,30 +218,30 @@ int main(int argc, char **argv) {
             
         /**/
             /* start the wait */
-            start_timer(); 
-            if (recvfrom(sockfd, buffer, MSS_SIZE, 0,
-                (struct sockaddr *) &clientaddr, (socklen_t *)&clientlen) < 0 && errno == EINTR) 
-            {
-                error("ERROR in recvfrom");
-            }
-            recvpkt = (tcp_packet *) buffer;
-            assert(get_data_size(recvpkt) <= DATA_SIZE);
-            if ( recvpkt->hdr.data_size == 0) /* if it was an empty packet, close program*/
-            {
-                sndpkt = make_packet(0);
-                if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
-                        (struct sockaddr *) &clientaddr, clientlen) < 0) {
-                    error("ERROR in sendto");
-                }
-                VLOG(INFO, "End Of File has been reached");
-                fclose(fp);
-                free(sndpkt);
-                break;
-            }
-            /* end the wait and restart the loop if you get another packet */
-            stop_timer(); 
+        //     start_timer(); 
+        //     if (recvfrom(sockfd, buffer, MSS_SIZE, 0,
+        //         (struct sockaddr *) &clientaddr, (socklen_t *)&clientlen) < 0 && errno == EINTR) 
+        //     {
+        //         error("ERROR in recvfrom");
+        //     }
+        //     recvpkt = (tcp_packet *) buffer;
+        //     assert(get_data_size(recvpkt) <= DATA_SIZE);
+        //     if ( recvpkt->hdr.data_size == 0) /* if it was an empty packet, close program*/
+        //     {
+        //         sndpkt = make_packet(0);
+        //         if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
+        //                 (struct sockaddr *) &clientaddr, clientlen) < 0) {
+        //             error("ERROR in sendto");
+        //         }
+        //         VLOG(INFO, "End Of File has been reached");
+        //         fclose(fp);
+        //         free(sndpkt);
+        //         break;
+        //     }
+        //     /* end the wait and restart the loop if you get another packet */
+        //     stop_timer(); 
 
-        /**/
+        // /**/
 
             
 
