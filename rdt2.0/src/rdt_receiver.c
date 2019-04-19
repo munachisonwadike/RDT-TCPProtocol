fa9469eea14b7d23b98f2a069e3aaa51bca017b7
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-04-19 08:41:44 +0400

    cleaning up the code

--- rdt2.0/src/rdt_receiver.c
+++ rdt2.0/src/rdt_receiver.c
@@ -132,9 +132,6 @@ void init_timer(int delay, void (*sig_handler)(int))
 
 
 int main(int argc, char **argv) {
-    VLOG(DEBUG, "VALUE OF DATA_SIZE! %lu",  DATA_SIZE);
-  
-
     /* 
      * check command line arguments 
      */
