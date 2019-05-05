92496c84f77cabed703f1034c39d49800b7b7e40
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-05-05 12:38:01 +0400

    ..

--- rdt2.0/src/rdt_receiver.c
+++ rdt2.0/src/rdt_receiver.c
@@ -182,7 +182,7 @@ int main(int argc, char **argv) {
             break;
         }
 
-        printf("JUST RECEIVED PACKET %d\n", recvpkt->hdr.seqno);
+        printf("JUST RECEIVED PACKET %d with flags %d \n", recvpkt->hdr.seqno, recvpkt->hdr.ctr_flags);
 
         /* 
          * sendto: ack back to the client 
@@ -219,8 +219,23 @@ int main(int argc, char **argv) {
 	        printf("sending ack (1) number %d\n", needed_pkt );  
 
             
+            /* 
+             * if the packet you receieved was the last packet, 
+             * exit the program
+             */
             
-                 
+             if ( recvpkt->hdr.ctr_flags == -2) { /* if it was empty packet, close the program */
+                sndpkt = make_packet(0);
+                sndpkt->hdr.ackno = -1;
+                if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
+                        (struct sockaddr *) &clientaddr, clientlen) < 0) {
+                    error("ERROR in sendto");
+                }
+                VLOG(INFO, "Just receieved last packet, exiting program");
+                fclose(fp);
+                free(sndpkt);
+                exit(0);
+            }    
              
 
 
--- rdt2.0/src/rdt_sender.c
+++ rdt2.0/src/rdt_sender.c
@@ -72,41 +72,41 @@ void resend_packets(int sig)
          * send the last packet 10 times just to 
          * be sure it makes it before closing out
          */
-        if (WINDOW_SIZE == 1){
+        // if (WINDOW_SIZE == 1){
                      
             
-            for (final_loop = 0; final_loop < FINAL_PACK_REPT; final_loop++)
-            {
-                if(sendto(sockfd, window[0], TCP_HDR_SIZE + get_data_size(window[0]), 0, 
-                ( const struct sockaddr *)&serveraddr, serverlen) < 0)
-                {
-                    error("sendto error");
-                }
-            }
-            free(window[0]); 
+        //     for (final_loop = 0; final_loop < FINAL_PACK_REPT; final_loop++)
+        //     {
+        //         if(sendto(sockfd, window[0], TCP_HDR_SIZE + get_data_size(window[0]), 0, 
+        //         ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+        //         {
+        //             error("sendto error");
+        //         }
+        //     }
+        //     free(window[0]); 
 
-            VLOG(INFO, "Window size is one, [RE]sending last packet ");
-            /*
-             * send 0 ack if nothing left to send in window 
-             * but send it 10 times just to be sure since there 
-             * is no way for receiver to know if it was recvd
-             */
-
-            for (final_loop = 0; final_loop < CLOSE_MESG_REPT; final_loop++)
-            {
-                sndpkt = make_packet(0);
-                sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
-                        (const struct sockaddr *)&serveraddr, serverlen);
-                free(sndpkt); 
-            }
-            exit(1);
-            VLOG(INFO, "[RE]sending 0 ack to receiver so it know to close ");
+        //     VLOG(INFO, "Window size is one, [RE]sending last packet ");
             
-        /*
-         * if there is more than one packet in the window, loop through 
-         * the packet window and send all the packets as usual
-         */
-        } else {
+        //      * send 0 ack if nothing left to send in window 
+        //      * but send it 10 times just to be sure since there 
+        //      * is no way for receiver to know if it was recvd
+             
+
+        //     for (final_loop = 0; final_loop < CLOSE_MESG_REPT; final_loop++)
+        //     {
+        //         sndpkt = make_packet(0);
+        //         sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
+        //                 (const struct sockaddr *)&serveraddr, serverlen);
+        //         free(sndpkt); 
+        //     }
+        //     exit(1);
+        //     VLOG(INFO, "[RE]sending 0 ack to receiver so it know to close ");
+            
+        // /*
+        //  * if there is more than one packet in the window, loop through 
+        //  * the packet window and send all the packets as usual
+        //  */
+        // } else {
 
             
             VLOG(DEBUG, "[RE]sending window of size %d from base %d -> %s", WINDOW_SIZE,  
@@ -126,7 +126,7 @@ void resend_packets(int sig)
      
             }
 
-        }
+        // }
 
 
     }
@@ -227,9 +227,17 @@ int main (int argc, char **argv)
 		len = fread(buffer, 1, DATA_SIZE, fp);
 		if (len<=0){
             window[window_index] = make_packet(0);
+ 
+            /* zero out the control flags since we will need them */
+            window[window_index]->hdr.ctr_flags = 0;
+ 
             if(len==0){
                 VLOG(INFO, "Empty file");
                 sndpkt = make_packet(0);
+
+                /* zero out the control flags since we will need them */
+                sndpkt->hdr.ctr_flags = 0;
+
                 sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
                         (const struct sockaddr *)&serveraddr, serverlen);
                 free(sndpkt); 
@@ -239,6 +247,10 @@ int main (int argc, char **argv)
             pkt_base = next_seqno;
         	next_seqno = pkt_base + len;
     		window[window_index] = make_packet(len);
+            
+            /* zero out the control flags since we will need them */
+            window[window_index]->hdr.ctr_flags = 0;
+
      		memcpy(window[window_index]->data, buffer, len);
             window[window_index]->hdr.seqno = pkt_base;
             
@@ -356,11 +368,16 @@ int main (int argc, char **argv)
                         WINDOW_SIZE = window_index;
                         VLOG(INFO, " End Of File. Window size %d ", WINDOW_SIZE);
                         final_packet_reached = 1;
+                        window[window_index-1]->hdr.ctr_flags = -2;
                         break;
                     }else{
                         pkt_base = next_seqno;
                         next_seqno = pkt_base + len; 
                         window[window_index] = make_packet(len);
+                        
+                        /* zero out the control flags since we will need them */
+                        window[window_index]->hdr.ctr_flags = 0;
+
                         memcpy(window[window_index]->data, buffer, len);
                         window[window_index]->hdr.seqno = pkt_base;
                         VLOG(DEBUG, "(2) generating window (size %d) with index %d, set to %d shift %d  ", 
@@ -404,45 +421,45 @@ int main (int argc, char **argv)
             
 
 
-            /* 
-             * send the last packet 10 times just to 
-             * be sure it makes it before closing out
-             */
-            if (WINDOW_SIZE == 1){
+            // /* 
+            //  * send the last packet 10 times just to 
+            //  * be sure it makes it before closing out
+            //  */
+            // if (WINDOW_SIZE == 1){
                          
                 
-                for (final_loop = 0; final_loop < FINAL_PACK_REPT; final_loop++)
-                {
-                    if(sendto(sockfd, window[0], TCP_HDR_SIZE + get_data_size(window[0]), 0, 
-                    ( const struct sockaddr *)&serveraddr, serverlen) < 0)
-                    {
-                        error("sendto error");
-                    }
-                }
-                free(window[0]); 
+            //     for (final_loop = 0; final_loop < FINAL_PACK_REPT; final_loop++)
+            //     {
+            //         if(sendto(sockfd, window[0], TCP_HDR_SIZE + get_data_size(window[0]), 0, 
+            //         ( const struct sockaddr *)&serveraddr, serverlen) < 0)
+            //         {
+            //             error("sendto error");
+            //         }
+            //     }
+            //     free(window[0]); 
 
-                VLOG(INFO, "Window size is one, sending last packet ");
-                /*
-                 * send 0 ack if nothing left to send in window 
-                 * but send it 10 times just to be sure since there 
-                 * is no way for receiver to know if it was recvd
-                 */
- 
-                for (final_loop = 0; final_loop < CLOSE_MESG_REPT; final_loop++)
-                {
-                    sndpkt = make_packet(0);
-                    sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
-                            (const struct sockaddr *)&serveraddr, serverlen);
-                    free(sndpkt); 
-                }
-                exit(1);
-                VLOG(INFO, "sending 0 ack to receiver so it know to close ");
+            //     VLOG(INFO, "Window size is one, sending last packet ");
                 
-            /*
-             * if there is more than one packet in the window, loop through 
-             * the packet window and send all the packets as usual
-             */
-            } else {
+            //      * send 0 ack if nothing left to send in window 
+            //      * but send it 10 times just to be sure since there 
+            //      * is no way for receiver to know if it was recvd
+                 
+ 
+            //     for (final_loop = 0; final_loop < CLOSE_MESG_REPT; final_loop++)
+            //     {
+            //         sndpkt = make_packet(0);
+            //         sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
+            //                 (const struct sockaddr *)&serveraddr, serverlen);
+            //         free(sndpkt); 
+            //     }
+            //     exit(1);
+            //     VLOG(INFO, "sending 0 ack to receiver so it know to close ");
+                
+            // /*
+            //  * if there is more than one packet in the window, loop through 
+            //  * the packet window and send all the packets as usual
+            //  */
+            // } else {
 
                 
                 VLOG(DEBUG, "sending window of size %d from base %d -> %s", WINDOW_SIZE,  
@@ -460,7 +477,7 @@ int main (int argc, char **argv)
          
                 }
 
-            }
+            // }
 
 
               
