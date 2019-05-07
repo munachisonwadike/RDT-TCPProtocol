5d80a2c9079f065efa443e18c58b750c518ca9b2
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-05-07 19:50:58 +0400

    ...

--- rdt2.0/src/rdt_receiver.c
+++ rdt2.0/src/rdt_receiver.c
@@ -229,7 +229,7 @@ int main(int argc, char **argv) {
              *  making sure to have first freed what was last-buffered steps behind.
              */
 
-            printf(" 1.5------  last-buffered = %d ---------1.5 \n", last_buffered);
+            printf(" 1.5------  last-buffered = %d && RCV_WIND_SIZE = %d---------1.5 \n", last_buffered, RCV_WIND_SIZE);
 
 
             window_index = 0; 
